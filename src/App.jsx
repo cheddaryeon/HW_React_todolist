0c2ec6cc2dc0bb80cf38ca5380b68a22096f912b
cheddaryeon <cheddaryeon@gmail.com>
2023-06-16 12:25:46 +0900

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,40 @@ import "./App.css";
 
 function App() {
   const id = Math.random();
-  const [work, setWork] = useState([
-    { id: id, title: "리액트 공부하기", comment: "리액트 기초를 공부해봅시다" },
-  ]);
+
   const [title, setTitle] = useState("");
   const [comment, setComment] = useState("");
+  const [work, setWork] = useState([
+    { id: id, title: "산책", comment: "강아지 산책 시키기" },
+  ]);
 
-  const titleChangeHandler = (event) => {
+  const getTitle = (event) => {
     setTitle(event.target.value);
   };
-  const commentChangeHandler = (event) => {
+  const getComment = (event) => {
     setComment(event.target.value);
   };
 
-  const clickAddButtonHandler = (event) => {
+  const addBtn = (event) => {
     event.preventDefault();
     const newWork = {
       id,
       title,
       comment,
     };
-    //배열유지
-    setWork([...work, newWork]);
+
+    setWork([...work, newWork]); //구글링 배열유지
+
     setTitle("");
     setComment("");
   };
 
-  const clickRemoveButtonHandler = (id) => {
+  const delBtn = (id) => {
     const newWorks = work.filter((work) => work.id !== id);
     setWork(newWorks);
   };
 
-  const clickCompleteButtonHandler = (id) => {
+  const comBtn = (id) => {
     const completeWorks = work.filter((work) => work.id !== id);
     const completeDoneWorks = work.filter((work) => work.id === id);
 
@@ -44,18 +46,18 @@ function App() {
 
   const [doneWorks, setDoneWorks] = useState([
     {
-      id: 1,
-      title: "리액트 공부하기",
-      comment: "리액트 기초를 공부해봅시다",
+      id: 0,
+      title: "자바스크립트",
+      comment: "자바스크립트 공부하기",
     },
   ]);
 
-  const clickDoneRemoveButtonHandler = (id) => {
-    const newDoneWorks = doneWorks.filter((donePlan) => donePlan.id !== id);
+  const doneDelBtn = (id) => {
+    const newDoneWorks = doneWorks.filter((doneWork) => doneWork.id !== id);
     setDoneWorks(newDoneWorks);
   };
 
-  const clickDeleteButtonHandler = (id) => {
+  const changeDoneBtn = (id) => {
     const deleteDoneWorks = doneWorks.filter((work) => work.id !== id);
     const deleteWorks = doneWorks.filter((work) => work.id === id);
 
@@ -65,50 +67,36 @@ function App() {
 
   return (
     <div className="layout">
-      <div className="container">
+      <div className="logo">
         <div>My Todo List</div>
         <div>React</div>
       </div>
       <form>
-        <div className="add-form">
-          <div className="input-group">
-            <label className="form-label">제목</label>
-            <input
-              className="input"
-              value={title}
-              onChange={titleChangeHandler}
-            />
+        <div className="adding">
+          <div className="search">
+            <label className="title-input">제목</label>
+            <input className="input" value={title} onChange={getTitle} />
 
-            <label className="form-label">내용</label>
-            <input
-              className="input"
-              value={comment}
-              onChange={commentChangeHandler}
-            />
+            <label className="title-input">내용</label>
+            <input className="input" value={comment} onChange={getComment} />
           </div>
-          <button className="add-button" onClick={clickAddButtonHandler}>
+          <button className="add-button" onClick={addBtn}>
             추가하기
           </button>
         </div>
       </form>
       <h2>Working.. 🔥</h2>
-      <div className="app-style">
+      <div className="section">
         {work.map((item) => {
           return (
-            <div key={item.id} className="component-style">
+            <div key={item.id} className="workcards">
               <h3>{item.title}</h3>
               <div>{item.comment}</div>
               <div className="btn">
-                <button
-                  className="red-btn"
-                  onClick={() => clickRemoveButtonHandler(item.id)}
-                >
+                <button className="red-btn" onClick={() => delBtn(item.id)}>
                   삭제하기
                 </button>
-                <button
-                  className="green-btn"
-                  onClick={() => clickCompleteButtonHandler(item.id)}
-                >
+                <button className="green-btn" onClick={() => comBtn(item.id)}>
                   완료
                 </button>
               </div>
@@ -118,22 +106,19 @@ function App() {
       </div>
 
       <h2>Done..! 🎉</h2>
-      <div className="app-style">
+      <div className="section">
         {doneWorks.map((item) => {
           return (
-            <div key={item.id} className="component-style">
+            <div key={item.id} className="workcards">
               <h3>{item.title}</h3>
               <div>{item.comment}</div>
               <div className="btn">
-                <button
-                  className="red-btn"
-                  onClick={() => clickDoneRemoveButtonHandler(item.id)}
-                >
+                <button className="red-btn" onClick={() => doneDelBtn(item.id)}>
                   삭제하기
                 </button>
                 <button
                   className="green-btn"
-                  onClick={() => clickDeleteButtonHandler(item.id)}
+                  onClick={() => changeDoneBtn(item.id)}
                 >
                   취소
                 </button>
